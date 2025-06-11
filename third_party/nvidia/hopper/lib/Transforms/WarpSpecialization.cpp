ce74be21670badfd409e14f1792512d9cf107fb1
Manman Ren <mren@meta.com>
2025-06-11 11:37:45 -0700

    address review comments
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- third_party/nvidia/hopper/lib/Transforms/WarpSpecialization.cpp
+++ third_party/nvidia/hopper/lib/Transforms/WarpSpecialization.cpp
@@ -3,6 +3,7 @@
 #include "mlir/Transforms/Passes.h"
 #include "nvidia/hopper/include/Transforms/Passes.h"
 #include "triton/Dialect/TritonGPU/IR/Dialect.h"
+#include "triton/Dialect/TritonGPU/Transforms/PipeliningUtility.h"
 
 #define DEBUG_TYPE "nvgpu-warp-specialization"
 #define DBGS() (llvm::dbgs() << "[" DEBUG_TYPE "]: ")
@@ -13,8 +14,7 @@ namespace mlir {
 void doTaskPartition(triton::FuncOp &funcOp, unsigned numWarpGroups);
 int doTaskIdPropagate(triton::FuncOp &funcOp);
 bool doDataPartition(triton::FuncOp &funcOp, unsigned numConsumerGroups);
-void doCodePartition(triton::FuncOp &funcOp, unsigned numBuffers,
-                     unsigned requestedRegisters);
+void doCodePartition(triton::FuncOp &funcOp, unsigned numBuffers);
 void doTokenLowering(triton::FuncOp &funcOp, unsigned numConsumerGroups);
 
 #define GEN_PASS_DEF_NVGPUWARPSPECIALIZATION
@@ -27,20 +27,30 @@ public:
       NVGPUWarpSpecializationPass>::NVGPUWarpSpecializationBase;
 
   void runOnFuncOp(triton::FuncOp funcOp) {
-    if (numWarpGroups <= 1)
+    SmallVector<scf::ForOp> loops;
+    funcOp->walk([&](scf::ForOp forOp) {
+      if (forOp->hasAttr(mlir::triton::kWarpSpecializeAttrName))
+        loops.push_back(forOp);
+    });
+    if (loops.empty())
       return;
 
-    // Partition key ops into multiple async tasks.
-    doTaskPartition(funcOp, numWarpGroups);
-    // Propagate taskId.
-    int retCode = doTaskIdPropagate(funcOp);
-    if (retCode == -1)
-      signalPassFailure();
+    unsigned numWarpGroups = 3;
+    for (; numWarpGroups >= 2; numWarpGroups--) {
+      // Partition key ops into multiple async tasks.
+      doTaskPartition(funcOp, numWarpGroups);
+      // Propagate taskId.
+      int retCode = doTaskIdPropagate(funcOp);
+      if (retCode == -1)
+        continue;
 
-    // Partition ops into parallel sub ops.
-    if (!doDataPartition(funcOp, numWarpGroups - 1))
-      signalPassFailure();
-    doCodePartition(funcOp, 2, 0); // FIXME
+      // Partition ops into parallel sub ops.
+      if (doDataPartition(funcOp, numWarpGroups - 1))
+        break;
+      // Clear async_task.
+    }
+
+    doCodePartition(funcOp, numStages);
     doTokenLowering(funcOp, numWarpGroups - 1);
   }
 
