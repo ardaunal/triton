ee6b31f6a9c5245d2c095134bea65425432c32c9
Manman Ren <mren@meta.com>
2025-06-11 12:33:39 -0700

    temp
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- third_party/nvidia/backend/compiler.py
+++ third_party/nvidia/backend/compiler.py
@@ -260,7 +260,7 @@ class CUDABackend(BaseBackend):
             passes.ttgpuir.add_combine_tensor_select_and_if(pm)
             passes.ttgpuir.add_assign_latencies(pm, opt.num_stages)
             passes.ttgpuir.add_schedule_loops(pm)
-            nvidia.passes.ttnvgpuir.add_hopper_warpspec(pm, opt.num_stages)
+            nvidia.passes.hopperws.add_hopper_warpspec(pm, opt.num_stages)
             passes.ttgpuir.add_pipeline(pm, opt.num_stages, dump_enabled)
         elif capability // 10 >= 10:
             passes.ttgpuir.add_fuse_nested_loops(pm)
